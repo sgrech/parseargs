91cad93bad8482701228b49b3d6508882e1322d5
Shane Grech <shanegrech@gmail.com>
2020-12-13 13:06:51 +0100

    Adding ParseArgs function & helpers

--- parseargs.go
+++ parseargs.go
@@ -9,46 +9,60 @@ import (
 type commands []command
 
 type command struct {
-	shortOption string
-	longOption  string
-	value       string
-	enabled     bool
+	option string
+	value  string
 }
 
-func (c commands) findCommand(opt string) (command, bool) {
+func (c commands) findCommand(opt string) (value string, ok bool) {
 	for _, comm := range c {
-		if comm.shortOption == opt || comm.longOption == opt {
-			return comm, true
+		if comm.option == opt {
+			return comm.value, true
 		}
 	}
-	return command{}, false
+	return "", false
 }
 
 func stripDashes(s string) string {
 	return strings.ReplaceAll(s, "-", "")
 }
 
-func parseArgs(args string) (commands, error) {
-	s := strings.Split(args, " ")
-	c := commands{}
-	for _, arg := range s {
+func parseLongOption(s string) (option string, value string) {
+	if strings.Contains(s, "=") {
+		kvp := strings.Split(s, "=")
+		return kvp[0], kvp[1]
+	}
+	return s, ""
+}
+
+func parseShortOption(s string) (options []string) {
+	if len(s) > 1 {
+		return strings.Split(s, "")
+	}
+	return []string{s}
+}
+
+func ParseArgs(args string) (c commands, err error) {
+	f := strings.Fields(args)
+	fmt.Println(f)
+	var comms commands
+	for _, arg := range f {
 		if strings.HasPrefix(arg, "--") {
-			c = append(c, command{
-				longOption: arg,
-				enabled:    true,
+			opt, val := parseLongOption(stripDashes(arg))
+			comms = append(comms, command{
+				option: opt,
+				value:  val,
 			})
 		} else if strings.HasPrefix(arg, "-") {
-			c = append(c, command{
-				shortOption: arg,
-				enabled:     true,
-			})
+			opts := parseShortOption(stripDashes(arg))
+			fmt.Println(arg, strings.HasPrefix(arg, "-"), opts)
+			for _, opt := range opts {
+				comms = append(comms, command{
+					option: opt,
+				})
+			}
 		} else {
 			return nil, errors.New(fmt.Sprintf("Arg %s has no dash prefix", arg))
 		}
 	}
-	return nil, nil
+	return comms, nil
 }
-
-// func (c *commands) RegisterArgs(args string) {
-
-// }
--- parseargs_test.go
+++ parseargs_test.go
@@ -18,24 +18,72 @@ func TestStripDashes(t *testing.T) {
 func TestFindCommand(t *testing.T) {
 	c := commands{
 		command{
-			shortOption: "a",
-			longOption:  "alpha",
+			option: "a",
 		},
 		command{
-			shortOption: "b",
-			longOption:  "bravo",
+			option: "bravo",
 		},
 		command{
-			shortOption: "c",
-			longOption:  "charlie",
+			option: "charlie",
+			value:  "cvalue",
 		},
 	}
 
 	if _, ok := c.findCommand("a"); !ok {
-		t.Fatalf("Expected arg \"a\" to be found")
+		t.Fatalf("Expected command \"a\" to be found")
 	}
 
 	if _, ok := c.findCommand("bravo"); !ok {
-		t.Fatalf("Expected arg \"bravo\" to be found")
+		t.Fatalf("Expected command \"bravo\" to be found")
+	}
+
+	if _, ok := c.findCommand("charlie"); !ok {
+		t.Fatalf("Expected command \"charlie\" to be found")
+	}
+}
+
+func TestParseLongOption(t *testing.T) {
+	if opt, _ := parseLongOption("alpha"); opt != "alpha" {
+		t.Fatalf("Expected %s but got %s", "alpha", opt)
+	}
+
+	if opt, val := parseLongOption("beta=betaval"); opt != "beta" && val != "betaval" {
+		t.Fatalf("Expected %s option and %s value but got %s option && %s value", "beta", "betaval", opt, val)
+	}
+}
+
+func TestParseShortOption(t *testing.T) {
+	opts := parseShortOption("a")
+	if len(opts) != 1 {
+		t.Fatalf("Expected slice of len 1 but got %d", len(opts))
+	}
+
+	if opts[0] != "a" {
+		t.Fatalf("Expected first element to be \"a\" but got \"%s\"", opts[0])
+	}
+
+	opts = parseShortOption("abc")
+
+	if len(opts) != 3 {
+		t.Fatalf("Expected slice of len 3 but got %d", len(opts))
+	}
+
+	if opts[0] != "a" && opts[1] != "b" && opts[2] != "c" {
+		t.Fatalf("Expected first three elements to be \"a\" \"b\" \"c\" but got \"%s\" \"%s\" \"%s\"", opts[0], opts[1], opts[2])
+	}
+}
+
+func TestParseArgs(t *testing.T) {
+	if cmds, err := ParseArgs("-a"); err == nil {
+		if len(cmds) != 1 {
+			t.Fatalf("Expected slice of len 1 but got %d", len(cmds))
+		}
+
+		if cmds[0].option != "a" {
+			t.Fatalf("Expected command with option \"a\"  but got \"%s\"", cmds[0].option)
+		}
+
+	} else {
+		t.Fatalf("Unexpected error %v", err)
 	}
 }
